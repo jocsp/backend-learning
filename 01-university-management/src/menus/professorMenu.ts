301dfec7617d3ca267c8905b8e0cb82aa26cf9ee
Jose Socorro <jcsocorro00@gmail.com>
2026-01-30 15:51:38 -0500

    01: Implement professor creation with validation

--- 01-university-management/src/menus/professorMenu.ts
+++ 01-university-management/src/menus/professorMenu.ts
@@ -1,8 +1,119 @@
 import { drawHomeMenu } from "./homeMenu.js";
 import { menu } from "../utils/menu.js";
+import { question } from "../utils/userInput.js";
+import { prisma } from "../prisma.js";
+import { validateDate } from "../utils/date.js";
+import { spacer } from "../utils/spacer.js";
 
-function addProfessor() {
-    console.log("Add new professor")
+async function addProfessor() {
+    let name: string, email: string, department: string, hireDate: string
+    // heading for the section
+    console.log("Add new professor: ")
+
+    // fetching the departments to display so the user can select the department to the user
+    const departments = await prisma.department.findMany({
+        select: {
+            id: true,
+            name: true,
+        }
+    })
+
+    while (true) {
+        name = await question("Professor name")
+
+        if (name.trim() == '') {
+            console.log("Name is empty, please input the name of the professor")
+            continue
+        }
+        break
+    }
+
+    while (true) {
+        email = await question("Professor email")
+
+        if (email.trim() == '') {
+            console.log("Email is empty, please input the email of the professor")
+            continue
+        }
+
+        break
+    }
+    console.log("")
+    console.log("Departments: ")
+
+    for (const department of departments) {
+        console.log(`${department.name} - id: ${department.id}`)
+    }
+
+    console.log("")
+    
+    while (true) {
+        department = await question("Professor Department (see list of departments above)")
+
+        const departmentExist = departments.some((d) => d.id == Number(department))
+
+        if (!departmentExist) {
+            console.log("The input department does not exist, please see the list above.")
+            continue
+        }
+
+        break
+    }
+
+    console.log("")
+
+    // variable that will contain the validated date
+    let validDate: Date
+
+    while (true) {
+        console.log("Please enter the date with the following format: yyyy-mm-DD (eg. 2026-01-30)")
+        console.log("Leave empty and hit enter if hire date is now")
+        hireDate = await question("Profesor hire date (hit enter if hire date is now)")
+
+        try {
+            validDate = validateDate(hireDate)
+            break
+        } catch(e: any) {
+
+            if (e instanceof Error) {
+                console.log(e.message)
+            } else {
+                console.log(e)
+            }
+            continue
+        }
+    }
+
+    // at this point all fields were collected and validated
+
+    const newProfessor = await prisma.professor.create({
+        data: {
+            name: name,
+            email: email,
+            departmentId: Number(department),
+            hireDate: validDate,
+        },
+        // only selecting name of the professor and department name
+        select: {
+            name: true,
+            department: {
+                select: {
+                    name: true
+                }
+            }
+        }
+    })
+
+    spacer(2)
+
+    console.log(`Successfully created ${newProfessor.name} under ${newProfessor.department.name} department`)
+
+    spacer(2)
+
+    await question("Press the Enter/Return key to continue")
+
+    drawHomeMenu()
+
 }
 
 function listProfessors() {
