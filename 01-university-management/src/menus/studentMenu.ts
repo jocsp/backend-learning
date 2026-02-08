787b228633dd62425889997c5e57a6ae336298e3
Jose Socorro <jcsocorro00@gmail.com>
2026-02-07 19:32:44 -0500

    01: Add register student functionality

--- 01-university-management/src/menus/studentMenu.ts
+++ 01-university-management/src/menus/studentMenu.ts
@@ -1,7 +1,90 @@
 import { drawHomeMenu } from "./homeMenu.js";
 import { menu } from "../utils/menu.js";
-function registerStudent() {
-    console.log("Register new student")
+import { spacer } from "../utils/spacer.js";
+import { question } from "../utils/userInput.js";
+import { validateDate } from "../utils/date.js";
+import { prisma } from "../prisma.js";
+
+async function registerStudent() {
+
+    let name: string, email: string, registrationDate: Date, major: string
+
+    spacer()
+    // heading title
+    console.log("Register New Student")
+
+    while (true) {
+        name = await question("Student name")
+
+        if (name.trim() == '') {
+            console.log("Name is empty, please input the name of the student")
+            continue
+        }
+        break
+    }
+
+    while (true) {
+        email = await question("Student email")
+
+        if (email.trim() == '') {
+            console.log("Email is empty, please input the email of the student")
+            continue
+        }
+        break
+    }
+
+    while (true) {
+        console.log("Please enter the date with the following format: yyyy-mm-DD (eg. 2026-01-30)")
+
+        const dateStr = await question("Student registration date (leave empty and hit enter if hire date is now)")
+
+        try {
+            registrationDate = validateDate(dateStr)
+            break
+        } catch(e: any) {
+
+            if (e instanceof Error) {
+                console.log(e.message)
+            } else {
+                console.log(e)
+            }
+            continue
+        }
+    }
+
+    while (true) {
+        major = await question("Major")
+
+        if (major.trim() == '') {
+            console.log("Major is empty, please input the major of the student")
+            continue
+        }
+        break
+    }
+
+    try {
+        const student = await prisma.student.create({
+            data: {
+                name: name,
+                email: email,
+                registrationDate: registrationDate,
+                major: major,
+            }
+        })
+
+        spacer()
+
+        console.log(`Student ${student.name} (${student.major}) has been succesfully registered.`)
+
+    } catch(e) {
+        console.log("Unexpected error occured", e)
+    }
+
+    spacer(2)
+
+    await question("Press Enter/Return key to continue")
+
+    drawHomeMenu()
 }
 
 function enrollStudentInCourse() {
